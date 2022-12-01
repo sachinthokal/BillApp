7fe92f88cac2723771ba8788c91819ac2696830d
sachinthokal <sachinthokal1799@gmail.com>
2022-12-01 17:29:25 +0530

    Update in Tabel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BillApp';
+
+  FormData: FormGroup;
+  Datalist: any;
+
+  ngOnInit() {
+    this.Datalist = [];
+
+    this.FormData = new FormGroup({
+      item: new FormControl(null),
+      qty: new FormControl(null),
+      rate: new FormControl(null),
+      discount: new FormControl(null),
+      mode: new FormControl(null),
+    });
+  }
+  Preview() {
+    console.log(this.FormData.value);
+  }
+  Add() {
+    // console.log(this.FormData);
+    this.Datalist.push(this.FormData.value);
+    this.FormData.reset();
+  }
+  delete(selecteddata: FormData) {
+    this.Datalist.forEach((value, index) => {
+      if (value == selecteddata) this.Datalist.splice(index, 1);
+      // console.log('Value', value);
+      // console.log('Index', index);
+      // console.log('selecteddata', selecteddata);
+    });
+  }
+
+  edit() {
+    alert('Edit');
+  }
 }

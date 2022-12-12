8cd54dfcb7b13fc246166ac72d97b0c1dbd87b5b
sachinthokal <sachinthokal1799@gmail.com>
2022-12-13 00:39:07 +0530

    Added Functionality on Buttons 13/12/2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
 
   FormData: FormGroup;
   Datalist: any;
-
+  key: boolean = true;
   ngOnInit() {
     this.Datalist = [];
 
@@ -43,5 +43,26 @@ export class AppComponent implements OnInit {
 
   edit() {
     alert('Edit');
+  }
+  onPreview() {
+    this.key = false;
+    console.log(this.Datalist);
+  }
+  Close() {
+    this.key = true;
+  }
+  print() {
+    var divToPrint = document.getElementById('print-index-invoice');
+    var newWin = window.open('', 'Print-Window');
+    newWin.document.open();
+    newWin.document.write(
+      '<html><link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" media="print"/><body onload="window.print()">' +
+        divToPrint.innerHTML +
+        '</body></html>'
+    );
+    newWin.document.close();
+    setTimeout(function () {
+      newWin.close();
+    }, 5);
   }
 }
